28a178f15782c60be33ab9b82dffebad22037e3a
arjungravi1@gmail.com <arjungravi1@gmail.com>
2025-03-18 17:47:14 +0530

    Patch

--- sparseAutoEncoders/monosemanticity_token_wise_checker.py
+++ sparseAutoEncoders/monosemanticity_token_wise_checker.py
@@ -1,10 +1,6 @@
 '''
 You can give a token and this code will run its activation through the SAE of your choice and will give the neurons that activated for each of those tokens.
 '''
-'''
-You can give a token and this code will run its activation through the SAE of your choice and will give the neurons that activated for each of those tokens.
-'''
-
 import torch
 import numpy as np
 from sae_jumprelu import JumpReluAutoEncoder
